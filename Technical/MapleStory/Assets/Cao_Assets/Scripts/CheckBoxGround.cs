f9a441aba6b148578b72db86a91053707dcbc948
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-05-24 20:16:05 +0700

    Tạo nhân vật di chuyển khi Click

---
+++ Technical/MapleStory/Assets/Cao_Assets/Scripts/CheckBoxGround.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CheckBoxGround : MonoBehaviour {
+
+
+    public List<Vector3> listBoxCheck;
+    public float distans;
+	// Use this for initialization
+	void Start () {
+        listBoxCheck = new List<Vector3>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    bool Distans(RectTransform taget)
+    {
+        float dis = Vector3.Distance(gameObject.transform.localPosition, taget.localPosition);
+        if (dis <= distans)
+            return true;
+        return false;
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        
+        if (col.tag == "Ground")
+        {
+            //Debug.Log("Da va cham voi Ground");
+            if (Distans((RectTransform)col.gameObject.transform))
+            {
+                if (!listBoxCheck.Contains(col.gameObject.transform.position))
+                    listBoxCheck.Add(col.gameObject.transform.position);
+            }
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.tag == "Ground")
+        {
+            if (listBoxCheck.Contains(col.gameObject.transform.position))
+            {
+                listBoxCheck.Remove(col.gameObject.transform.position);
+            }
+        }           
+    }
+    public void DuyetListBoxCheck()
+    {
+ 
+    }
+}
--- Technical/MapleStory/Assets/Cao_Assets/Scripts/PlayerController.cs
+++ Technical/MapleStory/Assets/Cao_Assets/Scripts/PlayerController.cs
@@ -9,27 +9,35 @@ public class PlayerController : BaseGameObject {
     public float moveForce = 365f;			// Amount of force added to move the player left and right.
     public float maxSpeed = 5f;
     public float jumpForce = 1000f;
-    private Transform groundCheck;
+    public GameObject boxJump;
+    public Transform groundCheck;
     private bool grounded = false;
-
+    private Vector3 posMouse;
+    private bool isMove;
+    private RaycastHit2D rayCat;
+    private CheckBoxGround _checkBoxGround;
+    
 	// Use this for initialization
 	void Start () {
-	
+        //rayCat = Physics2D.Raycast()
+        posMouse = Vector3.zero;
+        _checkBoxGround = gameObject.GetComponentInChildren<CheckBoxGround>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        //grounded = Physics2D.Linecast(transform.localPosition, groundCheck.localPosition, 1 << LayerMask.NameToLayer("Ground"));
+        MovePlayer();
 	}
     void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space) /*&& grounded*/)
-        {
-            isJumb = true;
-        }
+        //if (Input.GetKeyDown(KeyCode.Space) /*&& grounded*/)
+        //{
+        //    isJumb = true;
+        //}
         //grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        Move();
-        Shoot();
+        //Move();
+        //Shoot();
     }
     void Move()
     {
@@ -39,12 +47,12 @@ public class PlayerController : BaseGameObject {
             GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
         if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
             GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        if (h > 0 && facingRight)
+        if (h > 0 && !facingRight)
         {
             Flip();
         }
         
-        else if (h < 0 && !facingRight)
+        else if (h < 0 && facingRight)
             Flip();
         if (isJumb)
         {
@@ -82,5 +90,139 @@ public class PlayerController : BaseGameObject {
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
+    }
+    Transform rectPlayer = new RectTransform();
+    Vector3 worldPoint = Vector3.one;
+    void MovePlayer()
+    {        
+        Vector3 pos = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        //Debug.Log("POS PLayer = " + gameObject.transform.position);
+        
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            rectPlayer = gameObject.transform;
+            Debug.Log("Rect Player = " + rectPlayer.position);
+            worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Debug.Log(worldPoint);
+            //rayCat = Physics2D.Raycast(worldPoint, Vector3.one);
+            posMouse = Input.mousePosition;
+            Debug.Log("Pos Mouse = " + worldPoint);
+            isJumb = true;
+            isMove = true;
+            //if (rayCat.collider != null && rayCat.collider.gameObject.tag == "Ground")
+            //    SetBoxCollider(rectPlayer, (RectTransform)rayCat.collider.gameObject.transform);
+            if (pos.x < posMouse.x && !facingRight)
+            {
+                Flip();
+            }
+            if (pos.x > posMouse.x && facingRight)
+            {
+                Flip();                
+            }
+        }
+
+        float angle = AngleRotation(pos, posMouse);
+        float x = Mathf.Cos(Mathf.Deg2Rad * angle) * speed * Time.deltaTime;
+        if (isMove == true )
+        {
+            transform.localPosition += new Vector3(x, 0, 0);
+            if (DistanceClickMouse(rectPlayer, worldPoint))
+                CheckJump(worldPoint);
+            
+        }
+        
+    }
+
+    bool DistanceClickMouse(Transform _posPlayer, Vector3 _posMouse)
+    {
+        float _dis = Vector3.Distance(_posPlayer.localPosition, _posMouse);
+        Debug.Log("Dis = " + _dis);
+        if (_dis < 11)
+            return true;
+        return false;
+    }
+    void CheckJump(Vector3 posTouch)
+    {
+        for (int i = 0; i < _checkBoxGround.listBoxCheck.Count; i++)
+        {
+            if (posTouch.y > _checkBoxGround.listBoxCheck[i].y - 0.5 && posTouch.y < _checkBoxGround.listBoxCheck[i].y + 1.5)
+            {
+                if (posTouch.x > _checkBoxGround.listBoxCheck[i].x - 1 && posTouch.x < _checkBoxGround.listBoxCheck[i].x + 1)
+                {
+                      Jump();
+                }
+ 
+            }
+        }
+    }
+    float AngleRotation(Vector3 posBegin, Vector3 posEnd)
+    {
+        Vector3 relative = posEnd - posBegin;
+        float angle1 = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+        return angle1;
+    }
+    float AngleRotation1(RectTransform transfPlayer, RectTransform transfTouch)
+    {
+        Vector3 relative = new Vector3( (transfTouch.position.x - transfPlayer.position.x)/2,(transfTouch.position.y - transfPlayer.position.y )/ 2, 0);
+        float angle1 = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+        return angle1;
+    }
+    float Distansd(RectTransform posPlayer, RectTransform posTouch)
+    {
+        float distans = 0;
+        distans = Vector3.Distance(posPlayer.position, posTouch.position);
+        return distans;
+    }
+    [ContextMenu("Jump")]
+
+    void Jump()
+    {
+        if (isJumb == true)
+        {
+            Debug.Log("Player nhay");
+            rigid.AddForce(new Vector2(0f, jumpForce));
+            
+        }
+        if (rigid.velocity.y >= 0 && isJumb == true)
+        {
+            gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+            
+        }
+        else
+            if (rigid.velocity.y < 0)
+            {
+                gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+            }
+        isJumb = false;
+        
+    }
+    void Test()
+    {
+        
+    }
+    void SetBoxCollider(RectTransform transfPlayer, RectTransform transfTouch)
+    {
+        float lenght = Distansd(transfPlayer, transfTouch);
+        float _rotation = AngleRotation1(transfPlayer, transfTouch);
+
+
+        BoxCollider2D _boxJump = boxJump.GetComponent<BoxCollider2D>();
+        if (_boxJump != null)
+        {
+            
+            _boxJump.size = new Vector2(0.2f, lenght/2);
+            
+            boxJump.transform.position = new Vector3((transfPlayer.position.x + transfTouch.position.x) / 2, (transfPlayer.position.y + transfTouch.position.y)/ 2, 0);
+            boxJump.transform.rotation = Quaternion.Euler(0, 0, -_rotation);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
     }
 }

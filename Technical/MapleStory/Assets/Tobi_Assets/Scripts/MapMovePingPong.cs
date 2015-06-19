5578e409a61595fb03890962232dd750b9ba4064
tobi994 <viettt.fuky@gmail.com>
2015-06-19 16:12:08 +0700

    rescources map

---
+++ Technical/MapleStory/Assets/Tobi_Assets/Scripts/MapMovePingPong.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapMovePingPong : MonoBehaviour {
+
+    public float distance = 6.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.PingPong(Time.time, distance), transform.position.y, transform.position.z);
+	}
+}

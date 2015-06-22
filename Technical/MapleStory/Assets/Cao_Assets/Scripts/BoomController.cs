5e7da816116691016fa0557fcaa5d36c2da68087
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-06-22 15:04:26 +0700

    Lam boom, di chuyen cuc da

---
+++ Technical/MapleStory/Assets/Cao_Assets/Scripts/BoomController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoomController : BaseEnemyScripts {
+
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("no booom");
+        if (col.tag == "Player")
+        {
+            PlayerController _player = col.GetComponent<PlayerController>();
+            if (_player != null)
+            {
+                _player.Hit(damge);
+            }
+            _animator.SetBool("isAttack", true);
+        }
+    }
+}
--- Technical/MapleStory/Assets/Tobi_Assets/Scripts/MapMovePingPong.cs
+++ Technical/MapleStory/Assets/Tobi_Assets/Scripts/MapMovePingPong.cs
@@ -4,9 +4,56 @@ using System.Collections;
 public class MapMovePingPong : MonoBehaviour {
 
     public float distance = 6.0f;
-	
+    public float speed;
+    public bool isUp;
+    private Vector3 posStart;
+    private GameObject _player;
+    void Start()
+    {
+        posStart = transform.position;
+    }
 	// Update is called once per frame
 	void Update () {
-        //transform.position = new Vector3(Mathf.PingPong(Time.time, distance), transform.position.y, transform.position.z);	
+        Move();
+    }
+    void Move()
+    {
+        if (isUp == true)
+        {
+            transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+            if (Vector3.Distance(transform.position, posStart) > distance)
+                speed = -speed;
+        }
+        else
+        {
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            if (Vector3.Distance(transform.position, posStart) > distance)
+                speed = -speed;
+            
+        }
+        MovePlayer();
+    }
+    
+    void MovePlayer()
+    {
+        if (_player != null)
+        {
+            if (isUp == true)
+            {
+                _player.transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+            }
+            else
+            {
+                _player.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            }
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.tag == "Player")
+        {
+            _player = col.gameObject;
+        }
     }
 }

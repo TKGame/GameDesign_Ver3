30daba86c1cce8dc45a1504988e6143cfa65f892
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-06-27 00:35:37 +0700

    lam xong cai cau.

---
+++ Technical/MapleStory/Assets/Cao_Assets/Scripts/BridgeController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BridgeController : MonoBehaviour {
+
+    public float timeActive;
+    private float timeDelay;
+    private bool isActive;
+    public Vector3 Vantoc;
+
+
+	// Use this for initialization
+
+	void Start () {
+        
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveBridge();
+	}
+    void MoveBridge()
+    {
+        if (isActive == true)
+        {
+            if (timeDelay > timeActive)
+            {
+                transform.position += Vantoc;
+            }
+            timeDelay += Time.deltaTime;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == CTag.tagPlayer)
+        {
+            isActive = true;
+        }
+    }
+}
